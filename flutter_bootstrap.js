bc85f6f140f5586ed7895957bc9c17864cd6b971
Nidheesh krishna <nkrishna005@gmail.com>
2026-06-14 20:16:29 +0530

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3585445045"
+    serviceWorkerVersion: "4283283804"
   }
 });
